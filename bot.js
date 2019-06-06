0de683890508f3d83013c4030598082432c4f21a
EugeneArtsiukhevich <mailbox.aea@gmail.com>
2019-06-07 00:20:39 +0300

    make it better

--- bot.js
+++ bot.js
@@ -1,5 +1,18 @@
 const Telegraf = require('telegraf');
 const Markup = require('telegraf/markup');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
+});
 
 // initialize all dependencies
 require('./parser');
